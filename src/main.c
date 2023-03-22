080a52a1569601fc3b0523ac357b9ac7ad5f7c6d
francescomicucci <francesco.micucci@mail.polimi.it>
2023-03-22 13:45:09 +0100

    I/O changes

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
-unsigned int MAX=4294967295;
+unsigned int MAX = 4294967295;
+int scanReturnValue;
 
 typedef struct{
 	unsigned int cammino;
@@ -12,99 +13,32 @@ typedef struct{
 	unsigned int key;
 }nodo;
 
+void readNewGraph(int dim, int* notConnected, int (*matrix)[dim]);
+void printResults(int currentGraphNumber, int numberOfRankedElement, top maxheap[]);
+
 int main(){
-	char input[14], comando1[14]="AggiungiGrafo", numero[5000];
-	int d, k, q, n, i, j, valorebuttato, valore;
-	char* punt;
-	valorebuttato=scanf("%d %d", &d, &k);
-	top maxheap[k], temptop;
-	int matrice[d][d], numerografo=0, size=0, r, l , min, padre, somma=0, max_size=0, max_temp, nonconnesso=0; //shortcut=0, top, bottom;
-	nodo miniheap[d], u, temp;
+	char input[14], comando1[14]="AggiungiGrafo";
+	int dim, numberOfRankedElement, i, j;
+	scanReturnValue=scanf("%d %d", &dim, &numberOfRankedElement);
+	top maxheap[numberOfRankedElement], temptop;
+	int matrice[dim][dim], numerografo=0, size=0, r, l , min, padre, somma=0, max_size=0, max_temp, nonconnesso=0;
+	nodo miniheap[dim], u, temp;
 	
-	valorebuttato=scanf("%s", input);
+	scanReturnValue=scanf("%s", input);
 	ciclo:
 	while(input[0]==comando1[0]){
-		punt=fgets(numero, 5000, stdin);
-		for(i=0;i<d;i++){
-			if(i==0){
-				punt=fgets(numero, 5000, stdin);
-				valore=0;
-				j=0;
-				q=2;
-				while(numero[j]<='9' && numero[j]>='0'){
-					j++;
-				}
-				j++;
-				n=j;
-				while(numero[j]<='9' && numero[j]>='0'){
-					j++;
-				}
-				while(n<j){
-					valore=valore*10+(numero[n]-'0');
-					n++;
-				}
-				matrice[0][1]=valore;
-				n++;
-				j++;
-				while(q<d){
-					valore=0;
-					while(numero[j]<='9' && numero[j]>='0'){
-						j++;
-					}
-					while(n<j){
-						valore=valore*10+(numero[n]-'0');
-						n++;
-					}
-					if(valore!=matrice[0][1]){
-						nonconnesso=-1;
-					}
-					matrice[i][q]=valore;
-					n++;
-					if(numero[j]==','){
-						j++;
-					}
-					q++;
-				}
-			}
-			else{
-				if(nonconnesso==0){
-					punt=fgets(numero, 5000, stdin);
-				}
-				else{
-					punt=fgets(numero, 5000, stdin);
-					j=0;
-					q=0;
-					n=0;
-					while(q<d){
-						valore=0;
-						while(numero[j]<='9' && numero[j]>='0'){
-							j++;
-						}
-						while(n<j){
-							valore=valore*10+(numero[n]-'0');
-							n++;
-						}
-						matrice[i][q]=valore;
-						n++;
-						if(numero[j]==','){
-							j++;
-						}
-						q++;
-					}
-				}
-			}
-		}
+		readNewGraph(dim, &nonconnesso, matrice);
 		numerografo++;
 		if(nonconnesso==0){
-			somma=matrice[0][1]*(d-1);
+			somma=matrice[0][1]*(dim-1);
 			goto insert;
 		}
 		nonconnesso=0;
 		
-		size=d;
+		size=dim;
 		miniheap[0].key=0;
 		miniheap[0].num=0;
-		for(i=1;i<d;i++){
+		for(i=1;i<dim;i++){
 			miniheap[i].key=MAX;
 			miniheap[i].num=i;
 		}
@@ -154,7 +88,7 @@ int main(){
 	}
 
 		insert:
-		if(max_size<k){
+		if(max_size<numberOfRankedElement){
 			i=max_size;
 			max_size++;
 			maxheap[max_size-1].cammino=somma;
@@ -171,14 +105,14 @@ int main(){
 				maxheap[0].cammino=somma;
 				maxheap[0].numero_grafo=numerografo-1;
 				i=0;
-				while((2*(i+1)-1)<k){
+				while((2*(i+1)-1)<numberOfRankedElement){
 					l=(2*(i+1))-1;
 					r=(2*(i+1));
 					if(maxheap[l].cammino>maxheap[i].cammino){
 						max_temp=l;
 					}
 					else max_temp=i;
-					if(r<k && maxheap[r].cammino>maxheap[max_temp].cammino){
+					if(r<numberOfRankedElement && maxheap[r].cammino>maxheap[max_temp].cammino){
 						max_temp=r;
 					}
 					if(max_temp!=i){
@@ -189,39 +123,122 @@ int main(){
 						l=(2*(i+1))-1;
 						r=(2*(i+1));
 					}
-					else i=k;
+					else i=numberOfRankedElement;
 				}
 			}
 		}
 		somma=0;
-		valorebuttato=scanf("%s", input);
-		valorebuttato=*punt;
-
+		scanReturnValue=scanf("%s", input);
 	}
 
-	if(numerografo>k){
-		for(i=0;i<k-1;i++){
-			printf("%d ",maxheap[i].numero_grafo);
-		}
-		printf("%d",maxheap[k-1].numero_grafo);
-	}
-	else{
-		if(numerografo!=0){
-			for(i=0;i<numerografo-1;i++){
-			printf("%d ", i);
-			}
-			printf("%d", i);
-		}
-	}
-	valorebuttato=scanf("%s", input);
+	printResults(numerografo, numberOfRankedElement, maxheap);
+
+	scanReturnValue=scanf("%s", input);
 	if(input[0]==comando1[0]){
 		printf("\n");
 		goto ciclo;
 	}
 	
 	printf("\n");
-	if(valorebuttato==-2){
+	if(scanReturnValue==-2){
 		printf("Errore\n");
 	}
 	return 0;
+}
+
+void readNewGraph(int dim, int* notConnected, int (*matrix)[dim]){
+	int rowNum, colNum, characterPosition, n, value;
+	char* returnCondition;
+	char row[5000];
+
+	returnCondition = fgets(row, 5000, stdin);
+	for(rowNum = 0; rowNum < dim; rowNum++){
+		if(rowNum == 0){
+			returnCondition = fgets(row, 5000, stdin);
+			value = 0;
+			characterPosition = 0;
+			colNum = 2;
+			while(row[characterPosition] <= '9' && row[characterPosition] >= '0'){
+				characterPosition++;
+			}
+			characterPosition++;
+			n = characterPosition;
+			while(row[characterPosition] <= '9' && row[characterPosition] >= '0'){
+				characterPosition++;
+			}
+			while(n < characterPosition){
+				value = value*10+(row[n]-'0');
+				n++;
+			}
+			matrix[0][1] = value;
+			n++;
+			characterPosition++;
+			while(colNum < dim){
+				value = 0;
+				while(row[characterPosition] <= '9' && row[characterPosition] >= '0'){
+					characterPosition++;
+				}
+				while(n < characterPosition){
+					value = value * 10 + (row[n] - '0');
+					n++;
+				}
+				if(value != matrix[0][1]){
+					*notConnected =- 1;
+				}
+				matrix[rowNum][colNum] = value;
+				n++;
+				if(row[characterPosition] == ','){
+					characterPosition++;
+				}
+				colNum++;
+			}
+		}
+		else{
+			if(*notConnected == 0){
+				returnCondition = fgets(row, 5000, stdin);
+			}
+			else{
+				returnCondition = fgets(row, 5000, stdin);
+				characterPosition = 0;
+				colNum = 0;
+				n = 0;
+				while(colNum < dim){
+					value = 0;
+					while(row[characterPosition] <= '9' && row[characterPosition] >= '0'){
+						characterPosition++;
+					}
+					while(n < characterPosition){
+						value = value * 10 + (row[n] - '0');
+						n++;
+					}
+					matrix[rowNum][colNum] = value;
+					n++;
+					if(row[characterPosition] == ','){
+						characterPosition++;
+					}
+					colNum++;
+				}
+			}
+		}
+	}
+	scanReturnValue =* returnCondition;
+}
+
+void printResults(int currentGraphNumber, int numberOfRankedElement, top maxheap[]){
+	int counter;
+
+	if(currentGraphNumber > numberOfRankedElement){
+		for(counter = 0; counter < numberOfRankedElement - 1; counter++){
+			printf("%d ", maxheap[counter].numero_grafo);
+		}
+		printf("%d", maxheap[numberOfRankedElement - 1].numero_grafo);
+	}
+	else{
+		if(currentGraphNumber!=0){
+			for(counter = 0; counter < currentGraphNumber - 1; counter++){
+				printf("%d ", counter);
+			}
+			printf("%d", counter);
+		}
+	}
 }
